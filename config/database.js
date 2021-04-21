0a69e958f3560d5e0cd1ae19309ab8d87e815ab0
Tanecia Thompson <taneciathompson@gmail.com>
2021-04-20 20:19:02 -0400

    added config

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://mari27:code27@cluster0.3g7gm.mongodb.net/averyjames?retryWrites=true&w=majority", 
+    'dbName': 'averyjames'
+};
+// 'mongodb+srv://dashlin:sermeil15@cluster0.olf7t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
